ddf3ffcbb8d4d10e605d8cbd44557266c09bef87
trumpmcdonaldz <thetrumpmcdonaldz@gmail.com>
2025-06-17 02:48:43 +0800

    Implemented IntegerHelpers.cs

---
+++ NativeMemory/Helpers/IntegerHelpers.cs
@@ -0,0 +1,29 @@
+namespace NativeMemory.Helpers
+{
+    internal static class IntegerHelpers
+    {
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static int ToSignedUnchecked(this uint value)
+        {
+            return unchecked((int) value);
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static uint ToUnsignedUnchecked(this int value)
+        {
+            return unchecked((uint) value);
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static nuint ToNuintUnchecked(this int value)
+        {
+            return unchecked((nuint) value);
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static nint ToNintUnchecked(this uint value)
+        {
+            return unchecked((nint) value);
+        }
+    }
+}
